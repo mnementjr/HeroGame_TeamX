795b38d9f83c12f830dd111e2d26e0c6d7f6f313
XMelexus <58857939+XMelexus@users.noreply.github.com>
2020-06-07 13:35:20 +0300

    Created enum TypeOfDamage

---
+++ HeroGame/HeroGame/TypeOfDamage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroGame
+{
+    public enum TypeOfDamage
+    {
+        MagicDamage, PhysicalDamage, ShotDamage 
+    }
+}
